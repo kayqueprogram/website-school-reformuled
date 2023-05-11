b3e2ccaf47e6762ab79bf0a969f6787d767a4ff6
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-05-11 18:39:33 -0300

    teste

--- src/shared/layout/Footer/Footer.tsx
+++ src/shared/layout/Footer/Footer.tsx
@@ -2,20 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Footer = () => {
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+  
   return (
     <Container>
-      Footer
+      <div>Footer</div> - <div>footer</div>
+      <Button onClick={handleClick}>
+       ^
+      </Button>
     </Container>
   )
 }
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   padding: 16px;
   background-color: ${({ theme }) => theme.colors.details.primary.dark};
   color: ${({ theme }) => theme.colors.text.secondary.light};
+`;
+
+const Button = styled.button`
+  background-color: blue;
+  border: none;
+  cursor: pointer;
+  color: white;
 `;
 
 export default Footer
