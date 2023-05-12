b3b34b5afeb3a254ace2ca489bc16590af5c2969
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-05-11 21:10:56 -0300

    teste

--- src/pages/blog/post/Index.tsx
+++ src/pages/blog/post/Index.tsx
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components'
 import blogApi from 'services/blog';
 import { useParams } from 'react-router-dom';
@@ -9,22 +9,22 @@ import { breakStr } from 'shared/utils/breakStr';
 import Paragraph from 'shared/components/paragraph/Paragraph';
 
 const Index = () => {
-  const [post,setPost] = useState<any>({});
-  const {id} = useParams();
+  const [post, setPost] = useState<any>({});
+  const { id } = useParams();
 
-    useEffect(()=>{
-       const getPost = async() =>{
-           if(id){
-             const data = await blogApi.getBlogPost(id);
-             setPost(data);
-           }
-        };
+  useEffect(() => {
+    const getPost = async () => {
+      if (id) {
+        const data = await blogApi.getBlogPost(id);
+        setPost(data);
+      }
+    };
 
-        getPost();
-  },[id]);
+    getPost();
+  }, [id]);
 
-  
-  
+
+
   return (
     <>
       <Banner
@@ -33,42 +33,49 @@ const Index = () => {
         image={post?.image}
       />
       <SectionContainer
-       title={post?.title}
+        title={post?.title}
       >
-      {
-        post?.content && (
-         <Column gap='2.6rem'>
-           {breakStr(post.content)?.map(cont => <Paragraph>{cont}</Paragraph>)}
-         </Column>
-        )
-      } 
+        {
+          post?.content && (
+            <Column gap='2.6rem'>
+              {breakStr(post.content)?.map(cont => <Paragraph>{cont}</Paragraph>)}
+            </Column>
+          )
+        }
       </SectionContainer>
       {
         post?.vid1 && (
-                  <Video>
-                    <video controls src={post.vid1}></video>
-                  </Video>
-                )
+          <Container>
+            <Video>
+              <video controls src={post.vid1}></video>
+            </Video>
+          </Container>)
       }
-       {
+      {
         post?.vid2 && (
-                  <Video>
-                    <video controls src={post.vid2}></video>
-                  </Video>
-                )
+          <Container>
+            <Video>
+              <video controls src={post.vid2}></video>
+            </Video>
+          </Container>
+        )
       }
-       {
+      {
         post?.vid3 && (
-                  <Video>
-                    <video controls src={post.vid3}></video>
-                  </Video>
-                )
+          <Container>
+            <Video>
+              <video controls src={post.vid3}></video>
+            </Video>
+          </Container>
+        )
       }
-      {     post?.vid4 && (
-                  <Video>
-                    <video controls src={post.vid4}></video>
-                  </Video>
-                )
+      {post?.vid4 && (
+        <Container>
+          <Video>
+            <video controls src={post.vid4}></video>
+          </Video>
+        </Container>
+      )
       }
     </>
   )
@@ -87,5 +94,18 @@ const Video = styled.div`
     max-width: 100%;
   }
 `;
+
+const Container = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
+
+margin: auto;
+border-radius: 23px;
+background-color: ${({ theme }) => theme.colors.details.primary.dark};
+color: ${({ theme }) => theme.colors.text.secondary.light};
+padding: 20px;
+padding-top: 100px;
+`
 
 export default Index
