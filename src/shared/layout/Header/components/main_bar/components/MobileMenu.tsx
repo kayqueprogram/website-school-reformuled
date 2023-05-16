c96f48e8fa2b7e4f5cc67dd8f25d4e93de442bbb
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-05-16 19:04:59 -0300

    tamanho da fonte em navbar mobile

--- src/shared/layout/Header/components/main_bar/components/MobileMenu.tsx
+++ src/shared/layout/Header/components/main_bar/components/MobileMenu.tsx
@@ -15,7 +15,7 @@ const MobileMenu = () => {
       </IconBtn>
       {navIsOpen && (
         <NavField>
-          <Column gap="16px" alignItems="flex-end">
+          <Column gap="16px" alignItems="flex-end" width='100%'>
             <IconBtn onClick={() => setNavIsOpen(false)}>
               <pageIcons.Close />
             </IconBtn>
@@ -30,7 +30,7 @@ const MobileMenu = () => {
 const NavField = styled.div`
   position: fixed;
   width: 100%;
-  font-size: 250px;
+  
   left: 0;
   top: 0;
   height: 100vh;
@@ -38,6 +38,10 @@ const NavField = styled.div`
   text-align: right;
   background-color: ${({ theme }) => theme.colors.details.primary.dark};
   color: ${({ theme }) => theme.colors.text.secondary.light};
+
+  a {
+    font-size: 250px;
+  }
 `;
 
 export default MobileMenu;
