c3273336061cb0150c7ee1a5ae471c26456e0316
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-05-18 20:04:07 -0300

    ajustando player de audio

--- src/shared/components/galeria/audio/AudioPlayer.tsx
+++ src/shared/components/galeria/audio/AudioPlayer.tsx
@@ -1,76 +1,62 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-interface AudioPlayerProps {
-  src: string;
-}
+const AudioPlayerWrapper = styled.div`
+  background-color: #f8f8f8;
+  border-radius: 10px;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
-const AudioPlayer: React.FC<AudioPlayerProps> = ({ src }) => {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const audioRef = useRef<HTMLAudioElement>(null);
+const ControlsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ControlButton = styled.button`
+  background-color: #ffffff;
+  border: none;
+  border-radius: 50%;
+  height: 40px;
+  width: 40px;
+  margin: 0 10px;
+  cursor: pointer;
+`;
+
+const AudioPlayer = ({src}: {src:string}) => {
+  const [isPlaying, setIsPlaying] = React.useState(false);
+  const audioRef = React.useRef<HTMLAudioElement>(null);
 
   const togglePlay = () => {
-    const audio = audioRef.current;
-    if (!audio) return;
+    setIsPlaying(!isPlaying);
+  };
 
-    if (audio.paused) {
-      audio.play();
-      setIsPlaying(true);
-    } else {
-      audio.pause();
-      setIsPlaying(false);
+  const skipBackward = () => {
+    if (audioRef.current) {
+      audioRef.current.currentTime -= 10;
+    }
+  };
+
+  const skipForward = () => {
+    if (audioRef.current) {
+      audioRef.current.currentTime += 10;
     }
   };
 
   return (
-    <Container>
-      <Audio ref={audioRef} src={src} />
-      <Controls>
-        <PlayButton onClick={togglePlay}>
-          <Icon className={isPlaying ? 'pause' : 'play'} />
-        </PlayButton>
-      </Controls>
-    </Container>
+    <AudioPlayerWrapper>
+      <div>Áudio Player</div>
+      <ControlsWrapper>
+        <ControlButton onClick={skipBackward}>⏪</ControlButton>
+        <ControlButton onClick={togglePlay}>{isPlaying ? '⏸️' : '▶️'}</ControlButton>
+        <ControlButton onClick={skipForward}>⏩</ControlButton>
+      </ControlsWrapper>
+      <audio ref={audioRef} src={src} />
+    </AudioPlayerWrapper>
   );
 };
-
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const Audio = styled.audio`
-  display: none;
-`;
-
-const Controls = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const PlayButton = styled.button`
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-  padding: 0;
-  display: flex;
-  align-items: center;
-`;
-
-const Icon = styled.div`
-  width: 20px;
-  height: 20px;
-  background-color: #333;
-  border-radius: 50%;
-  margin-right: 8px;
-
-  &.play {
-    background-color: #333;
-  }
-
-  &.pause {
-    background-color: #f00;
-  }
-`;
 
 export default AudioPlayer;
