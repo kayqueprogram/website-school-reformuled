c02243f7a07334ae30b81621429592d34cc769db
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-05-18 22:42:13 -0300

    atualização de galeria

--- src/pages/galeria/DiaD.tsx
+++ src/pages/galeria/DiaD.tsx
@@ -13,7 +13,7 @@ const GalleryWrapper = styled.div`
 
   margin: auto;
   border-radius: 23px;
-  background-color: ${({theme})=> theme.colors.details.primary.dark};
+  background-color: ${({ theme }) => theme.colors.details.primary.dark};
   color: ${({ theme }) => theme.colors.text.secondary.light};
   padding: 20px;
   padding-top: 100px;
@@ -30,8 +30,20 @@ const ImageWrapper = styled.div`
   }
 `;
 
+const Container = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+
+margin: auto;
+border-radius: 23px;
+color: ${({ theme }) => theme.colors.text.secondary.light};
+padding: 20px;
+padding-top: 100px;
+`
+
 const DiaD = () => {
-  const images = ['https://cdn.discordapp.com/attachments/1098057370728403115/1104185974839324782/IMG-20230505-WA0004.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100910943002693732/20230426_192622.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908447865774201/IMG-20230426-WA0010.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908447622512670/IMG-20230426-WA0009.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908447391813652/IMG-20230426-WA0012.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908447114993724/IMG-20230426-WA0013.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101276495743307816/IMG-20230427-WA0282.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908446854955038/IMG-20230426-WA0011.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908446586507404/IMG-20230426-WA0014.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908446200635512/IMG-20230426-WA0015.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908445852516413/IMG-20230426-WA0016.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908296577232997/IMG-20230426-WA0026.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101283331397656637/IMG-20230427-WA0297.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908296187150386/IMG-20230426-WA0025.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908295876784168/IMG-20230426-WA0029.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908295574782013/IMG-20230426-WA0030.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908295281197177/IMG-20230426-WA0027.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101266972093726750/IMG-20230427-WA0269.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101267408330690620/IMG-20230427-WA0270.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101267941535776818/IMG-20230427-WA0272.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101267941812617299/IMG-20230427-WA0273.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101267942127173682/IMG-20230427-WA0274.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101268527538782299/IMG-20230427-WA0275.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101268527803015319/IMG-20230427-WA0276.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101268528079847444/IMG-20230427-WA0277.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101269274334593045/IMG-20230427-WA0278.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101269274594660532/IMG-20230427-WA0281.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101269274884059176/IMG-20230427-WA0280.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101269275118927942/IMG-20230427-WA0279.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185972083667015/IMG-20230505-WA0021.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185974189203516/IMG-20230505-WA0006.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185974453440542/IMG-20230505-WA0005.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185973652328508/IMG-20230505-WA0009.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185973392285696/IMG-20230505-WA0011.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185973086105652/IMG-20230505-WA0014.jpg' ,'https://cdn.discordapp.com/attachments/1098057370728403115/1103438306060009533/IMG-20230503-WA0002.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1103438305514754178/IMG-20230503-WA0000.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1103438305770623149/IMG-20230503-WA0003.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1103438306366197801/IMG-20230503-WA0001.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185972725387324/IMG-20230505-WA0016.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185972398235861/IMG-20230505-WA0017.jpg' ];
+  const images = ['https://cdn.discordapp.com/attachments/1098057370728403115/1104185974839324782/IMG-20230505-WA0004.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100910943002693732/20230426_192622.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908447865774201/IMG-20230426-WA0010.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908447622512670/IMG-20230426-WA0009.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908447391813652/IMG-20230426-WA0012.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908447114993724/IMG-20230426-WA0013.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101276495743307816/IMG-20230427-WA0282.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908446854955038/IMG-20230426-WA0011.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908446586507404/IMG-20230426-WA0014.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908446200635512/IMG-20230426-WA0015.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908445852516413/IMG-20230426-WA0016.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908296577232997/IMG-20230426-WA0026.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101283331397656637/IMG-20230427-WA0297.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908296187150386/IMG-20230426-WA0025.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908295876784168/IMG-20230426-WA0029.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908295574782013/IMG-20230426-WA0030.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1100908295281197177/IMG-20230426-WA0027.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101266972093726750/IMG-20230427-WA0269.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101267408330690620/IMG-20230427-WA0270.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101267941535776818/IMG-20230427-WA0272.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101267941812617299/IMG-20230427-WA0273.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101267942127173682/IMG-20230427-WA0274.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101268527538782299/IMG-20230427-WA0275.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101268527803015319/IMG-20230427-WA0276.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101268528079847444/IMG-20230427-WA0277.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101269274334593045/IMG-20230427-WA0278.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101269274594660532/IMG-20230427-WA0281.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101269274884059176/IMG-20230427-WA0280.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1101269275118927942/IMG-20230427-WA0279.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185972083667015/IMG-20230505-WA0021.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185974189203516/IMG-20230505-WA0006.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185974453440542/IMG-20230505-WA0005.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185973652328508/IMG-20230505-WA0009.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185973392285696/IMG-20230505-WA0011.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185973086105652/IMG-20230505-WA0014.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1103438306060009533/IMG-20230503-WA0002.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1103438305514754178/IMG-20230503-WA0000.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1103438305770623149/IMG-20230503-WA0003.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1103438306366197801/IMG-20230503-WA0001.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185972725387324/IMG-20230505-WA0016.jpg', 'https://cdn.discordapp.com/attachments/1098057370728403115/1104185972398235861/IMG-20230505-WA0017.jpg'];
 
 
   const [post, setPost] = React.useState<any>({});
@@ -52,21 +64,41 @@ const DiaD = () => {
 
   return (
     <div>
-        <GalleryWrapper>
-          {images.map((image, index) => (
-              <ImageWrapper key={index}>
-                <img src={image} alt={`Image ${index}`} />
-              </ImageWrapper>
-          
-          ))}
-          <Title>Vídeos</Title>
-          <Videos />
-          <Title>Àudios</Title>
-          <AudioPlayer src={post.aud3} title={post.titleAud3} />
-          <AudioPlayer src={post.aud3} title={post.titleAud3} />
-          <AudioPlayer src={post.aud3} title={post.titleAud3} />
-          <AudioPlayer src={post.aud3} title={post.titleAud3} />
-        </GalleryWrapper>
+      <GalleryWrapper>
+        {images.map((image, index) => (
+          <ImageWrapper key={index}>
+            <img src={image} alt={`Image ${index}`} />
+          </ImageWrapper>
+
+        ))}
+        <Title>Vídeos</Title>
+        <Videos />
+        <Title>Àudios</Title>
+        {post?.aud1 && (
+          <Container>
+            <AudioPlayer src={post.aud1} title={post.titleAud1} />
+          </Container>
+        )
+        }
+        {post?.aud2 && (
+          <Container>
+            <AudioPlayer src={post.aud2} title={post.titleAud2} />
+          </Container>
+        )
+        }
+        {post?.aud3 && (
+          <Container>
+            <AudioPlayer src={post.aud3} title={post.titleAud3} />
+          </Container>
+        )
+        }
+        {post?.aud4 && (
+          <Container>
+            <AudioPlayer src={post.aud4} title={post.titleAud4} />
+          </Container>
+        )
+        }
+      </GalleryWrapper>
     </div>
   );
 };
