fabd686280ea5181adc64155f09c28ec8cceaecf
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-05-19 18:37:34 -0300

    teste

--- src/pages/about/Index.tsx
+++ src/pages/about/Index.tsx
@@ -2,31 +2,31 @@ import React, { useState, useEffect } from 'react';
 import aboutApi from 'services/about';
 import { useParams } from 'react-router-dom';
 
-const About = () => {
-
-    const [about, setAbout] = useState<any>({});
-    const { id } = useParams();
-
-    useEffect(() => {
-        const getAbout = async () => {
-            if (id) {
-                const data = await aboutApi.getAboutSchool(id);
-                setAbout(data);
-            }
-        };
-
-        getAbout();
-    }, [id]);
-
-    return (
-        <>
-            {
-                about?.title && (
-                    <div>{about.title}</div>
-                )
-            }
-        </>
-    )
+interface AboutData {
+  title: string;
+  // adicione outras propriedades do objeto 'about' aqui
 }
 
-export default About
+const About: React.FC = () => {
+  const [about, setAbout] = useState<AboutData | null>(null);
+  const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    const getAbout = async () => {
+      if (id) {
+        const data = await aboutApi.getAboutContent(id);
+        setAbout(data);
+      }
+    };
+
+    getAbout();
+  }, [id]);
+
+  return (
+    <>
+      {about?.title && <div>{about.title}</div>}
+    </>
+  );
+};
+
+export default About;
--- src/services/about/get.ts
+++ src/services/about/get.ts
@@ -1,23 +1,37 @@
 import { collection, doc, getDocs, getDoc } from 'firebase/firestore';
 import { db } from 'services/firebase-config';
 
-interface AboutData {
-  id: string;
-  title: string;
-  description: string;
+
+async function getAbout() {
+  try {
+    const postsCollectionRef = collection(db, 'about');
+    const response = await getDocs(postsCollectionRef);
+    const data = response.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    return data;
+  } catch (error) {
+    console.error('Erro ao buscar :', error);
+    return [];
+  }
 }
 
-async function getAbout(): Promise<AboutData[]> {
-  const aboutCollectionRef = collection(db, "about");
-  const response = await getDocs(aboutCollectionRef);
-  const data = response.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as AboutData[];
-  return data;
+// Função para buscar um post específico
+async function getAboutContent(id: string) {
+  try {
+    const postRef = doc(db, 'about', id);
+    const postDoc = await getDoc(postRef);
+    
+    if (postDoc.exists()) {
+      // Documento encontrado, você pode acessar os dados usando postDoc.data()
+      const data = postDoc.data();
+      return data;
+    } else {
+      console.log('Documento não encontrado');
+      return null;
+    }
+  } catch (error) {
+    console.error('Erro ao buscar o post:', error);
+    return null;
+  }
 }
 
-async function getAboutSchool(id: string): Promise<AboutData | undefined> {
-  const aboutRef = doc(db, 'about', id);
-  const aboutDoc = await getDoc(aboutRef);
-  return aboutDoc.data() as AboutData | undefined;
-}
-
-export default { getAbout, getAboutSchool };
+export default { getAbout, getAboutContent };
