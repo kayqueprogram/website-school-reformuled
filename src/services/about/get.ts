0ffca5b054908952a7c2c7ec59a664f5664bb764
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-05-19 17:36:03 -0300

    teste

--- src/services/about/get.ts
+++ src/services/about/get.ts
@@ -1,21 +1,23 @@
 import { collection, doc, getDocs, getDoc } from 'firebase/firestore';
 import { db } from 'services/firebase-config';
 
-
-async function getAbout() {
-    const aboutCollectionRef = collection(db, "about");
-    const response = await getDocs(aboutCollectionRef);
-    const data = await response.docs?.map((doc) => ({ ...doc.data(), id: doc.id }))
-    return data;
+interface AboutData {
+  id: string;
+  title: string;
+  description: string;
 }
 
-async function getAboutSchool(id: string) {
-    const aboutRef = doc(db, 'about', id);
-    const aboutDoc = await getDoc(aboutRef);
-    return aboutDoc.data()
-
+async function getAbout(): Promise<AboutData[]> {
+  const aboutCollectionRef = collection(db, "about");
+  const response = await getDocs(aboutCollectionRef);
+  const data = response.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as AboutData[];
+  return data;
 }
 
-
+async function getAboutSchool(id: string): Promise<AboutData | undefined> {
+  const aboutRef = doc(db, 'about', id);
+  const aboutDoc = await getDoc(aboutRef);
+  return aboutDoc.data() as AboutData | undefined;
+}
 
 export default { getAbout, getAboutSchool };
