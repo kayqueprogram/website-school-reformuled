75e6ef22634e20316c3dd74aade1a74ed9aecf40
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-06-22 21:24:27 -0300

    fonts

--- src/shared/components/title/Title.tsx
+++ src/shared/components/title/Title.tsx
@@ -11,9 +11,11 @@ const Title = ({children}:{children:React.ReactNode}) => {
 
 const Container = styled.h1`
   width: 92%;
+  left: 0;
   font-size: 2.9rem;
-  font-weight: 700;
+  font-weight: 900;
   font-size: clamp(1em, 1.6em + 1.5vw, 3.3em);
+  margin-bottom: 10px;
 `;
 
 export default Title
