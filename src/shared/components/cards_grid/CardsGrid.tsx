b9ab3162098dc6dfed695e7a2f54c29f8f11b6f0
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-08-13 09:58:08 -0300

    commit

--- src/shared/components/cards_grid/CardsGrid.tsx
+++ src/shared/components/cards_grid/CardsGrid.tsx
@@ -1,86 +1,84 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Grid from '../grid/Grid';
 import ReactPaginate from 'react-paginate';
-import Column from '../column/Column';
+import Column from '../column/Column'; // Make sure to import this
 import pageIcons from 'shared/utils/pageIcons';
 
+
 type TProps = {
-    CardComponent:React.FC<any>,
-    data:Array<any>
+  CardComponent: React.FC<any>,
+  data: Array<any>
 }
 
-const CardsGrid = ({CardComponent,data}:TProps) => {
+const CardsGrid = ({ CardComponent, data }: TProps) => {
   const [pageNumber, setPageNumber] = useState(0);
 
   const dataPerPage = 10;
   const pagesVisited = pageNumber * dataPerPage;
   const pageCount = Math.ceil(data.length / dataPerPage);
-  const dataToRend = data.slice(pagesVisited, pagesVisited + dataPerPage);
+  const dataToRender = data.slice(pagesVisited, pagesVisited + dataPerPage);
 
-  const changePage = ({ selected }:{selected:number}):void => {
+  const changePage = ({ selected }: { selected: number }): void => {
     setPageNumber(selected);
   };
 
   return (
     <Container>
       <Column>
-      <Grid>
-        {
-          dataToRend.map((el)=><CardComponent {...el}/>)
-        }
-      </Grid>
-      <PaginateField>
-        <ReactPaginate 
-          previousLabel={<pageIcons.ArrowLeft/>}
-          nextLabel={<pageIcons.ArrowRight/>}
-          pageCount={pageCount}
-          onPageChange={changePage}
-          containerClassName={"pagination"}
-          pageLinkClassName={'btn'}
-          disabledClassName={"paginationDisabled"}
-          activeClassName={"activeBtn"} 
-        />
-      </PaginateField>
-    </Column>
+        <Grid>
+          {
+            dataToRender.map((el) => <CardComponent key={el.id} {...el} />) // Added key prop
+          }
+        </Grid>
+        <PaginateField>
+          <ReactPaginate
+            previousLabel={<pageIcons.ArrowLeft />}
+            nextLabel={<pageIcons.ArrowRight />}
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName={"pagination"}
+            pageLinkClassName={'btn'}
+            disabledClassName={"paginationDisabled"}
+            activeClassName={"activeBtn"}
+          />
+        </PaginateField>
+      </Column>
     </Container>
   )
 }
 
 const Container = styled.div`
- padding: 3rem 0rem;
+  padding: 3rem 0rem;
 `;
 
 const PaginateField = styled.div`
- width: 100%;
-display: flex;
-justify-content: flex-end;
- padding: 4rem 0px;
-
-
- .pagination{
+  width: 100%;
   display: flex;
-  align-items: center;
-  list-style: none;
+  justify-content: flex-end;
+  padding: 4rem 0px;
 
-  .btn,.next,.previous,.activeBtn{
-    width: 36px;
-    height: 36px;
+  .pagination {
     display: flex;
-    justify-content: center;
     align-items: center;
-    justify-content: center;
-    align-items: center;
-    border: 1px solid gainsboro;
-    cursor: pointer;
+    list-style: none;
+
+    .btn, .next, .previous, .activeBtn {
+      width: 36px;
+      height: 36px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      justify-content: center;
+      align-items: center;
+      border: 1px solid gainsboro;
+      cursor: pointer;
+    }
+
+    .activeBtn {
+      background-color: gainsboro;
+    }
   }
-
-  .activeBtn{
-    background-color: gainsboro;
-  }
-
- }
-
 `;
 
 export default CardsGrid;
