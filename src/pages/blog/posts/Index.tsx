3f9c720be94d7c41841ee3a056f2b96dccc10be0
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-11-06 19:26:28 -0300

    teste

--- src/pages/blog/posts/Index.tsx
+++ src/pages/blog/posts/Index.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import blogApi from 'services/blog';
 import BlogCard from 'shared/components/blog_card/BlogCard';
-import SectionContainer from 'shared/components/Section_Container/SectionContainer';
+import RowToColumn from 'shared/components/row_to_column/RowToColumn';
 import Banner from 'shared/components/banner/Banner';
+import SectionContainer from 'shared/components/Section_Container/SectionContainer';
 import CardsGrid from 'shared/components/cards_grid/CardsGrid';
 import DataFetcher from 'shared/components/data_fetcher/DataFetcher';
 import { Link } from 'react-router-dom';
@@ -22,7 +23,7 @@ const Index = () => {
                 <SectionContainer>
                     <DataFetcher
                         fetchFunc={blogApi.getBlogPosts}
-                        Component={SectionContainer}
+                        Component={RowToColumn}
                         propsForComponent={{ CardComponent: BlogCard, }}
                     />
                 </SectionContainer>
